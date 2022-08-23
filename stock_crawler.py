1e8289fa009a74ba699c8c9dd653f4d1ddc2e655
Anthony Lee <hososlch@gmail.com>
2022-08-23 15:28:38 +0800

    update: AMZN case

--- stock_crawler.py
+++ stock_crawler.py
@@ -1,3 +1,5 @@
+from typing import List, Any
+
 import numpy as np
 import pandas as pd
 import yfinance as yf
@@ -6,14 +8,18 @@ from macrotrends_crawler import MacrotrendsCrawler
 
 
 class StockCrawler:
+    features: list[str]
     symbol: str
     crawler: MacrotrendsCrawler
+    df: pd.DataFrame
 
     def __init__(self, symbol: str):
         self.symbol = symbol
         self.crawler = MacrotrendsCrawler(symbol)
+        self.df = pd.DataFrame()
+        self.features = []
 
-    def fundamentals(self) -> pd.DataFrame:
+    def add_fundamentals(self):
         pe_table = self.crawler.price_earnings_ratio()
         ps_table = self.crawler.price_sales_ratio()
         pfcf_table = self.crawler.price_fcf_ratio()
@@ -21,13 +27,36 @@ class StockCrawler:
         fundamentals['EPS'] = fundamentals['TTM Net EPS'].replace('', np.nan).astype(float)
         fundamentals['SPS'] = fundamentals['TTM Sales per Share'].replace('', np.nan).astype(float)
         fundamentals['FCF'] = fundamentals['TTM FCF per Share'].replace('', np.nan).astype(float)
-        return fundamentals[['EPS', 'SPS', 'FCF']].dropna()
 
-    def prices(self) -> pd.DataFrame:
-        return yf.Ticker(self.symbol).history(period='max')
+        self.merge(fundamentals[['EPS', 'SPS', 'FCF']].dropna())
+
+        return self
+
+    def merge(self, data: pd.DataFrame):
+        if self.df.empty:
+            self.df = data
+        else:
+            self.df = self.df.merge(data, left_index=True, right_index=True, how='outer')
+        return self
+
+    def add_prices(self):
+        prices = yf.Ticker(self.symbol).history(period='max')
+        self.merge(prices[['Close']].dropna())
+        return self
+
+    def add_feature(self, symbol: str, rolling_period: int = 1):
+        prices = yf.Ticker(symbol).history(period='max')
+        prices[symbol] = prices['Close'].rolling(rolling_period).mean().dropna()
+        self.merge(prices[[symbol]])
+        self.features.append(symbol)
+        return self
+
+    def add_features(self, symbols: [str], rolling_period: int = 21):
+        for symbol in symbols:
+            self.add_feature(symbol, rolling_period)
+        return self
 
     def table(self) -> pd.DataFrame:
-        df = self.prices().merge(self.fundamentals(), left_index=True, right_index=True, how='outer')
-        df.fillna(method='ffill', inplace=True)
-        df.dropna(inplace=True)
-        return df
+        self.df.fillna(method='ffill', inplace=True)
+        self.df.dropna(inplace=True)
+        return self.df
---
+++ success-case.md
@@ -0,0 +1,9 @@
+# AMZN
+
+training_upto: '2017-12-31'
+
+features:
+- SPS
+- UUP (MA 21)
+
+---
