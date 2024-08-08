e06c5b2579ca2953e238ee26dc8dee0204b434d8
Anthony Lee <anthonylee1994@users.noreply.github.com>
2024-08-08 18:36:35 +0800

    refactor: add requirements.txt

--- fetch_data.py
+++ fetch_data.py
@@ -3,7 +3,7 @@ import yfinance as yf
 def fetch_data(ticker):
     try:
         stock = yf.Ticker(ticker)
-        data = stock.history(period='1mo', interval='15m')
+        data = stock.history(period='1mo', interval='5m')
         return data
     except Exception:
         return None
